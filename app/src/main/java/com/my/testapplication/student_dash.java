b025f604aa7b0ba2ffa1ee879917b26bad4ac0a7
Amulkumar.Babalad <r.palimkar@live.com>
2019-09-08 12:56:55 +0530

    Initial Commit

---
+++ app/src/main/java/com/my/testapplication/student_dash.java
@@ -0,0 +1,16 @@
+package com.my.testapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class student_dash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student_dash);
+
+
+    }
+}
